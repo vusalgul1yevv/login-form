737c77bbbf41acce9e8ac103476abc498c3d558c
VusalGuliyevv <vsalquliyev@gmail.com>
2024-08-22 00:55:14 +0400

    the registration form has been prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
-import AddUser from './components/Register'
+import "./App.css";
+import AddUser from "./components/Register";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
